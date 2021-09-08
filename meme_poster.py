02c7e4541350a47c9eddc1eb2b094bc394558de0
Siddhesh Zantye <siddheshadsv@icloud.com>
2021-09-08 21:47:27 +1200

    Make it run 5 times instead of 1

--- meme_poster.py
+++ meme_poster.py
@@ -6,11 +6,13 @@ import requests
 import dotenv
 import glob
 
+# Delete them damm cookies that keep comming
 cookie_del = glob.glob("config/*cookie.json")
 os.remove(cookie_del[0])
 
 dotenv.load_dotenv()
 
+# Create reddit client
 def reddit_client():
     client = praw.Reddit(
         client_id= os.environ['CLIENT_ID'],
@@ -19,6 +21,7 @@ def reddit_client():
     )
     return client
 
+# Get top 50 image urls (memes)
 def get_img_url(client: praw.Reddit, sub_name: str, limit: int):
     hot_memes = client.subreddit(sub_name).hot(limit=limit)
     image_urls = []
@@ -26,20 +29,27 @@ def get_img_url(client: praw.Reddit, sub_name: str, limit: int):
         image_urls.append(post.url)
     return image_urls
 
+# Get the urls
 client = reddit_client()
 urls = get_img_url(client=client, sub_name='memes', limit=50)
 
-image = random.choice(urls)
-
-response = requests.get(image)
-with open('image.png', 'wb') as f:
-    f.write(response.content)
-    f.close()
-
+# Make insta bot
 bot = bot.Bot()
 
+# Login
 bot.login(username=os.environ['insta_user'], password=os.environ['insta_pass'])
-image = 'image.png'
 
-bot.upload_photo(image, caption='Test, pls work')
-print("Done")
+# Choose a random image and save it
+amount = 5
+for i in range(amount):
+    image = random.choice(urls)
+    response = requests.get(image)
+    with open('image.png', 'wb') as f:
+        f.write(response.content)
+        f.close()
+
+    image = 'image.png'
+
+    # Upload photo
+    bot.upload_photo(image, caption='Meme lmao')
+    print("Done")
