2f90ba147a669e230cf57ac8ed439780c1a4a380
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2021-12-24 12:22:11 +1300

    Commit

--- new.py
+++ new.py
@@ -1,14 +1,27 @@
 import json
+import os
 
 urls = []
 
 ays = input("Are you sure you want to create these files? y/n: ")
 
 if ays.lower() == "y":
-    with open('urls.json', 'w') as f:
+    with open('url.json', 'w') as f:
         json.dump(urls, f)
         
-    with open('log.txt', 'w') as f:
+    with open('logs.txt', 'w') as f:
         f.write("--Log File--")
+
+    cid = input("Please enter your client id:")
+    os.system("clear")
+    cs = input("Please enter your client secret:")
+    os.system("clear")
+    iu = input("Please enter your instagram username:")
+    os.system("clear")
+    ip = input("Please enter your instagram password:")
+    os.system("clear")
+
+    with open('.ennv', 'w') as f:
+        f.write()
 else:
     print("Ok")
