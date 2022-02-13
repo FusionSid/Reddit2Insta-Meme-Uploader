a2d9907b30f26f667fe9fe8a7e644a412ead1eef
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2022-02-13 16:05:47 +1300

    Updates Bot, Post titles, Post author credit working and more effiecent code

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import json
 import time
 from instabot import bot
 import praw
-import dotenv
-import glob
+from dotenv import load_dotenv
 import wget
 from datetime import datetime
+import shutil
 
+load_dotenv()
 
 # Logging cause i like too.
+
+
 def log(log):
     now = datetime.now()
     timern = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -21,25 +24,27 @@ def log(log):
         f.write('\n')
         f.write(f"{timern} | {log}")
 
+
 # For Deleting them damm cookies and other shit that keep comming
 def deletecookies():
     try:
-        cookie_del = glob.glob("config/*cookie.json")
-        os.remove(cookie_del[0])
+        shutil.rmtree("config")
         print("Cookies Eaten Successfuly.")
         log("Cookies Eaten Successfuly.")
-    except:
+    except Exception as e:
         print("Cookies Deletion Failed.")
-        log("Cookies Deletion Failed.")
+        log(f"Cookies Deletion Failed. {e}")
+
 
 # Create reddit client
 def reddit_client():
     client = praw.Reddit(
-        client_id= os.environ['CLIENT_ID'],
-        client_secret = os.environ['CLIENT_SECRET'],
-        user_agent = "memes-fastapi"
+        client_id=os.environ['CLIENT_ID'],
+        client_secret=os.environ['CLIENT_SECRET'],
+        user_agent="memes-fastapi"
     )
     return client
+
 
 # Check if url is an image
 def is_image(post):
@@ -48,35 +53,40 @@ def is_image(post):
     except AttributeError:
         return False
 
-# Get top 100 image urls (memes)
-def get_img_url(client: praw.Reddit, sub_name: str, limit: int):
-    hot_memes = client.subreddit(sub_name).hot(limit=limit)
-    image_urls = []
-    for post in hot_memes:
-        if is_image(post):
-            image_urls.append(post.url)
 
-    return image_urls
+# Get top 100 image urls (memes)
+def get_img_url(client: praw.Reddit, subreddits: list, limit: int):
+    memes = []
+    for sub_name in subreddits:
+        hot_memes = client.subreddit(sub_name).hot(limit=limit)
+
+        for post in hot_memes:
+            if is_image(post):
+                data = {
+                    "url": post.url,
+                    "author": post.author.name,
+                    "title": post.title
+                }
+                memes.append(data)
+
+    return memes
 
 
 log("----------START----------")
+# Notification for mac, If youre not on mac delete this line
+os.system("""osascript -e 'display notification "Starting Meme Uploads" with title "Reddit 2 Insta"'""")
 deletecookies()
-dotenv.load_dotenv()
+
 
 # Create reddit client
 client = reddit_client()
 
-urls = []
-
 # get 100 image urls from each of these subreddits
 rpsnlist = ['memes', 'dankmemes']
-for sub_reddit in rpsnlist:
-    subred = sub_reddit
-    url = get_img_url(client=client, sub_name=subred, limit=100) 
-    urls.append(url)
-    
+memes = get_img_url(client=client, subreddits=rpsnlist, limit=100)
+
 log("Downloaded urls")
-    
+
 # Make insta bot
 bot = bot.Bot()
 
@@ -85,44 +95,51 @@ bot.login(username=os.environ['insta_user'], password=os.environ['insta_pass'])
 
 log("Logged In Successfully!")
 
-# Choose an image and save it
-ncount = 0
-ocount = 0
-gcount = 0
-ht = "[#memes #funny #reddit #dankmemes #lol #memesdaily #humor #dank #meme #followorgetrickrolled #image #random #images]" # hastags
-for item in urls:
-    for item in item:
-        # get file extension jpg, png, gif, etc
-        fileext = item[-4] + item[-3] + item[-2] + item[-1]
-        with open('urls.json', 'r') as f:
-            data = json.load(f)
-        if item in data:
-            pass  
-            ocount += 1
-        else:
-            data.append(item)
-            with open('urls.json', 'w') as f:
-                json.dump(data, f, indent=4)
-            ncount += 1
-            if fileext == '.gif':
-                pass
-                gcount += 1
-            else:
-                filename = wget.download(url=str(item))
-                # Upload photo
-                try:
-                    bot.upload_photo(filename, caption=f'Subreddit: {subred}\nCredit: {item}\n\n{ht}')
-                    time.sleep(2)
-                except Exception as e:
-                    print(f"Error: {e}")
-                    log(f"Error: {e}")
-                os.remove(filename)
+hashtags = "#memes #funny #reddit #dankmemes #lol #memesdaily #humor #dank #meme #followorgetrickrolled #image #random #images"
+
+old_count, new_count, gif_count = 0, 0, 0
+
+for meme in memes:
+    with open('urls.json', 'r') as f:
+        data = json.load(f)
+
+    post_url = meme["url"]
+    post_author = meme["author"]
+    post_title = meme["title"]
+
+    fileext = post_url[-4] + post_url[-3] + post_url[-2] + post_url[-1]
+    if fileext == '.gif':
+        gif_count += 1
+        continue
+
+    if post_url in data:
+        old_count += 1
+        continue
+    else:
+        data.append(post_url)
+        new_count += 1
+        with open("urls.json", 'w') as f:
+            json.dump(data, f, indent=4)
+
+    filename = wget.download(url=str(post_url), out="upload")
+
+    try:
+        bot.upload_photo(
+            filename, caption=f"{post_title}\nAuthor: {post_author}\n\n{hashtags}")
+        time.sleep(2)
+    except Exception as e:
+        log(f"Error: {e}")
+    os.remove(filename)
 
 # Log
-log(f"{ncount}/200 new urls")
-log(f"{ocount}/200 old urls")
-log(f"{gcount}/200 gifs")
+log(f"{new_count}/{len(rpsnlist)*100} new urls")
+log(f"{old_count}/{len(rpsnlist)*100} old urls")
+log(f"{gif_count}/{len(rpsnlist)*100} gifs")
 
 deletecookies()
-os.system("""osascript -e 'display notification "Finished" with title "Reddit 2 Insta"'""")
+
+# Notification for mac, If youre not on mac delete this line
+os.system(
+    """osascript -e 'display notification "Finished" with title "Reddit 2 Insta"'""")
+
 log("-----------END-----------")
